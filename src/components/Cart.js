ac5eaa9cca1070c062b2cb2a2bd4bd786408ab5e
valery-chumak <blossssom88@gmail.com>
2023-11-11 16:17:37 +0200

    basic functionality added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Cart.css";
+import { useSelector } from "react-redux";
 const Cart = () => {
-  const quantity = 5;
+  const quantity = useSelector((state) => state.cart.totalQuantity);
+
   return (
     <div className="cartIcon">
       <h3>Cart: {quantity} Items</h3>
